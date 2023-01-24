79eaa94df4d88f6bf7cddd4aa1edcb95d4e269f0
sardor-web-21 <sardor06sultonov21@gmail.com>
2023-01-24 19:08:30 +0500

    fayldagi kamchiliklar yoqotildi

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import "./assets/styles/index.css";
-import Aside from "./components/Aside/data";
-import { Header } from "./components/Header/UserCard";
-import { Footer } from "./components/Footer/Todo";
-import { Section } from "./components/Section/Section";
+// import Aside from "./components/Aside/data";
+// import Header  from "./components/Header/UserCard";
+// import Footer  from "./components/Footer/Todo";
+// import Section  from "./components/Section/Section";
 
 const App = () => {
   return (
-    <>
-      <Header />
-      <Aside />
-      <Footer />
-      <Section />
-    </>
+    <div className="wrapper">
+      <div className="left">
+        <h1>salom</h1>
+        {/* <Aside /> */}
+      </div>
+      <div className="right">
+        {/* <Header /> */}
+        {/* <Section /> */}
+        {/* <Footer /> */}
+      </div>
+    </div>
   );
 };
 export default App;
